1128f41a90b83eb3a31aabc50b2c31966b661c0f
matheus <matheus.silveiraw@gmail.com>
2024-07-09 16:49:48 -0300

    aula 27 factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            UserSeeder::class
-        ]);
+
+        $this->call(CategoriasSeeder::class);
+
+        // $this->call([
+        //     // UserSeeder::class
+        //     CategoriasSeeder::class, 
+        // ]);
     }
 }
