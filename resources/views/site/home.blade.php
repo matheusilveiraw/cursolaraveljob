b54af4c63e38f2ad51fedab2000cf91fe5bb49eb
matheus <matheus.silveiraw@gmail.com>
2024-07-11 12:14:14 -0300

    aula 36

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -10,4 +10,59 @@
 
 {{$teste ?? 'padrão'}} {{-- ?? serve para dar um valor padrão para a variavel, que nesse caso não existe no controler --}}
 
+
+{{-- estruturas de controle dentro do blade --}}
+
+{{-- if --}}
+
+@if ($nome == 'rodrigo') 
+    true 
+@else 
+    false
+@endif
+
+{{-- unless, oposto do if --}}
+
+@unless ($nome == 'rodrigo') 
+    true 
+@else 
+    false
+@endunless
+
+{{-- switch --}}
+
+@switch($idade)
+    @case(28)
+        idade está ok
+        @break
+    @case(30)
+        idade n tá ok
+        @break
+    @default
+        algum problema com a idade
+@endswitch
+
+{{-- isset --}}
+
+@isset($nome)
+    existe
+@endisset
+
+{{-- empty checa se está vazio--}}
+
+@empty($nome)
+    está vazia
+@endempty
+
+{{-- auth - vÊ se tem um usuario autenticado --}}
+@auth
+    está autenticado
+@endauth
+
+{{-- oposto do auth, retorna se não ter um usuario autenticado --}}
+@guest
+    não tem usuário autenticado
+@endguest
+
+
 @endsection
