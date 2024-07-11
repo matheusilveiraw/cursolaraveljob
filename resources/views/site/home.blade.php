5e51ea41365b89ab81ba294797c2453d56634fb3
matheus <matheus.silveiraw@gmail.com>
2024-07-11 14:40:06 -0300

    aula 41

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,7 +4,61 @@
 
 @section('conteudo')
 
-<h1>Essa é nossa home</h1>
+<div class="row container">
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>    
+    </div>
 
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+    
+    </div>
+
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+    
+    </div>
+
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+    
+    </div>
+</div>
     
 @endsection
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -5,11 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <!-- Compiled and minified CSS -->
-
-    @stack('style')
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-    @yield('conteudo')
+    <nav class="red">
+        <div class="nav-wrapper container">
+          <a href="#" class="brand-logo center">Curso Laravel </a>
+          <ul id="nav-mobile" class="left">
+            <li><a href="###">Home</a></li>
+            <li><a href="###">Carrinho</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      @yield('conteudo')
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
