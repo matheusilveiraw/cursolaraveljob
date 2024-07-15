24a85cf1d0164b530f451d2bb9cd2a85d0ac63e4
matheus <matheus.silveiraw@gmail.com>
2024-07-15 14:37:51 -0300

    aula 61

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -9,33 +9,45 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <style>
-      .badge.blue {
-          color: white !important;
-      }
-  </style>
+        .badge.blue {
+            color: white !important;
+        }
+    </style>
 </head>
 
 <body>
     <nav>
         <div class="nav-wrapper container">
             <a href="#" class="brand-logo center">Curso Laravel </a>
+
             <ul id="nav-mobile" class="left">
                 <li><a href="{{ route('site.index') }}">Home</a></li>
                 <li><a href="#" class="dropdown-trigger" data-target="dropdown1">Categorias <i
                             class="material-icons right">expand_more</i> </a></li>
+                <li><a href="{{ route('site.carrinho') }}">Carrinho<span class="badge blue" data-badge-caption="">
+                            {{ Cart::getContent()->count() }}</span> </a></li>
+            </ul>
 
-                <!-- Dropdown Structure -->
-                <ul id='dropdown1' class='dropdown-content'>
-                    @foreach ($categoriasMenu as $categoriaM)
-                        <li><a href="{{ route('site.categoria', $categoriaM->id) }}">{{ $categoriaM->nome }}</a></li>
-                    @endforeach
-                </ul>
-
-
-                <li><a href="{{ route('site.carrinho') }}">Carrinho<span class="badge blue" data-badge-caption=""> {{ Cart::getContent()->count() }}</span> </a></li>
+            <ul id="nav-mobile" class="right">
+                <li><a href="#" class="dropdown-trigger" data-target="dropdown2">Olá, {{ auth()->user()->firstName }} <i
+                            class="material-icons right">expand_more</i> </a></li>
             </ul>
         </div>
     </nav>
+
+
+<!-- Dropdown Structure - dropdown do nav-mobile left -->
+    <ul id='dropdown1' class='dropdown-content'>
+        @foreach ($categoriasMenu as $categoriaM)
+            <li><a href="{{ route('site.categoria', $categoriaM->id) }}">{{ $categoriaM->nome }}</a></li>
+        @endforeach
+    </ul>
+
+    {{-- dropdown do usuario  --}}
+    <ul id='dropdown2' class='dropdown-content'>
+            <li><a href="{{ route('admin.dashboard')}}">Dashboard</a></li>
+    </ul>
+
 
 
     @yield('conteudo')
